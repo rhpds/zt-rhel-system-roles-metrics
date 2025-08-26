1feb83762740479587efe30f0427e2dfb7c238b3
Matthew Yee <myee@redhat.com>
2025-08-26 08:54:46 -0700

    Configure cockpit service in setup-rhel.sh to enable web functionality and set permissions for SSH config.

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -8,3 +8,9 @@ Host *
 EOF
 
 chmod 400 /root/.ssh/config
+
+# Enable cockpit functionality in showroom.
+echo "[WebService]" > /etc/cockpit/cockpit.conf
+echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
+echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
+systemctl enable --now cockpit.socket
